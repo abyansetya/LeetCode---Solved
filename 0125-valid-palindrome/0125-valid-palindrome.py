545708943f284c6ad6126b21d104aed2b436235b
abyan setyaneva <127168084+abyansetya@users.noreply.github.com>
2024-03-14 17:20:23 +0700

    Time: 44 ms (62.64%), Space: 18.1 MB (20.49%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,6 +1,22 @@
 import re
 
-class Solution:
+class Solution: 
+    
+    
     def isPalindrome(self, s: str) -> bool:
+        # with build in function
+        # memory is not O(1)
         clean = re.sub(r'[^a-zA-Z0-9]', '', s.lower())
         return clean == clean[::-1]
+        
+        #Memory O(1)
+#         l,r = 0, len(s)-1
+        
+#         while l < r:
+#             while l < r and not self.alphanum(s[l]):
+#                 l += 1
+#             while r > l and not self.alphanum(s[r]):
+#                 r -=1
+#             if s[l].lower() != s[r].lower():
+#                 return False
+#         return True
