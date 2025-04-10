68cdd204ce608ffb6b8d4586dd99b74a5a74c74e
abyan setyaneva <127168084+abyansetya@users.noreply.github.com>
2025-04-10 22:25:56 +0700

    Time: 15 ms (10.31%), Space: 24.2 MB (13.31%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,15 +1,32 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        freq = [[] for i in range(len(nums)+1)]
+        # map = {}
+
+        # for num in nums:
+        #     if num in map:
+        #         map[num] += 1
+        #     else:
+        #         map[num] = 1
         
-        for n in nums:
-            count[n] = 1 + count.get(n,0)
-        for n,c in count.items():
-            freq[c].append(n)
+        # sorted_map = sorted(map.items(), key=lambda x:x[1], reverse=True)
+
+        # result = []
+        # for i in range(k):
+        #     result.append(sorted_map[i][0])
+
+        # return result
+        map = {}
+        bucket = [[] for _ in range(len(nums) + 1)]
+
+        for num in nums:
+            map[num] = 1 + map.get(num, 0)
+        
+        for num, freq in map.items():
+            bucket[freq].append(num)
+
         res = []
-        for i in range(len(freq)-1,0,-1):
-            for n in freq[i]:
-                res.append(n)
-                if(len(res) == k):
+        for i in range(len(bucket)-1, 0, -1):
+            for num in bucket[i]:
+                res.append(num)
+                if len(res) == k:
                     return res
