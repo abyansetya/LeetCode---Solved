badbf2a1330a71f0a9a7226670dd9080f1c54e5b
abyan setyaneva <127168084+abyansetya@users.noreply.github.com>
2025-04-13 00:47:25 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,22 +1,17 @@
-import re
-
-class Solution: 
-    
-    
+class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # with build in function
-        # memory is not O(1)
-        clean = re.sub(r'[^a-zA-Z0-9]', '', s.lower())
-        return clean == clean[::-1]
-        
-        #Memory O(1)
-#         l,r = 0, len(s)-1
-        
-#         while l < r:
-#             while l < r and not self.alphanum(s[l]):
-#                 l += 1
-#             while r > l and not self.alphanum(s[r]):
-#                 r -=1
-#             if s[l].lower() != s[r].lower():
-#                 return False
-#         return True
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while l < r and s[l] is not s[l].isalnum():
+                l +=1
+            while l < r and s[r] is not s[r].isalnum():
+                r -=1
+            
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            l += 1
+            r -= 1
+    
+        return True
