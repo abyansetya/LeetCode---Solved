279ceb18e191f4c80f79e1562a13fa3535a2e708
abyan setyaneva <127168084+abyansetya@users.noreply.github.com>
2026-07-01 22:13:20 +0700

    Time: 1 ms (91.08%), Space: 58.1 MB (8.91%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -2,48 +2,22 @@
  * @param {string} s
  * @return {boolean}
  */
-
- //brute force
-// var isValid = function(s) {
-//     let stack = []
-
-//     for(let i = 0; i < s.length; i++){
-//         if(s[i] === '(' || s[i] === '{' || s[i] === '['){
-//             stack.push(s[i])
-//         }else{
-//             if(stack.length === 0 || 
-//             (s[i] === ')' && stack[stack.length - 1] !== '(') ||
-//             (s[i] === '}' && stack[stack.length- 1] !== '{') ||
-//             (s[i] === ']' && stack[stack.length - 1] !== '[')
-//             ){
-//                 return false;
-//             }
-//             stack.pop();
-//         }
-//     }
-
-//     return stack.length == 0
-// };
-
-// using hashmap
 var isValid = function(s) {
-    let stack = [];
-    let closeToOpen = {
-        ')': '(',
-        '}': '{',
-        ']': '['
-    };
+    let map = {")" : "(", "}": "{", "]" : "["}
+    const res = []
 
-    for (let c of s) {
-        if (closeToOpen.hasOwnProperty(c)) {
-            if (stack.length > 0 && stack[stack.length - 1] === closeToOpen[c]) {
-                stack.pop();
-            } else {
-                return false;
-            }
-        } else {
-            stack.push(c);
+    for(letter of s){
+        if(letter == "(" || letter == "[" || letter == "{" ){
+            res.push(letter)
+        }
+        else{
+            const top = res.pop()
+            if(top !== map[letter]) return false
         }
     }
-    return stack.length === 0;
+
+    return res.length == 0
 };
+
+//map = {")": "(", "}": "{", "]" : "["}
+//if(set.first[]  )
